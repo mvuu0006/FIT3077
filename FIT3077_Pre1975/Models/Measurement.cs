88d477e160a6b5cd78a0cb572c51d253eccf6881
Duc Nguyen <dngu0019@student.monash.edu>
2020-05-08 16:10:46 +1000

    Add messy stuff

---
+++ FIT3077_Pre1975/Models/Measurement.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FIT3077_Pre1975.Models
+{
+    public class Measurement
+    {
+
+        public int Value { get; set; }
+
+        public string Unit { get; set; }
+    }
+}
--- FIT3077_Pre1975/Models/Practitioner.cs
+++ FIT3077_Pre1975/Models/Practitioner.cs
@@ -1,12 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace FIT3077_Pre1975.Models
 {
-    public class Practitioner : Person
+    public class Practitioner
     {
-        
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public Gender Gender { get; set; }
+
+        [Display(Name = "Date of Birth")]
+        [DataType(DataType.Date)]
+        public DateTime? BirthDate { get; set; }
+
+        public Address Address { get; set; }
     }
 }
